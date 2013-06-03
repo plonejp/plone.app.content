2a2914aa31674368d169fafc8a9cae114910a3fa
Andy Leeb <ableeb@andersonleeb.com>
2013-06-03 15:05:45 -0500

    upload dexterity file factory test still failing

--- plone/app/content/browser/tests/test_fileuploadcreation.py
+++ plone/app/content/browser/tests/test_fileuploadcreation.py
@@ -1,28 +1,34 @@
 import unittest2 as unittest
 from plone.app.content.browser.interfaces import IATCTFileFactory
 from plone.app.content.browser.interfaces import IDXFileFactory
-from plone.app.content.browser.factories import ATCTFileFactory
-from plone.app.content.browser.factories import DXFileFactory
-from Products.CMFCore.interfaces._content import IFolderish
 from Products.ATContentTypes.interfaces import IATFile
+from plone.dexterity.interfaces import IDexterityFTI
+
 from Products.PloneTestCase import PloneTestCase as ptc
 
-ptc.setupPloneSite()
+ptc.installPackage('plone.app.dexterity')
+ptc.setupPloneSite(extension_profiles=('plone.app.dexterity:default',))
+
+class DXFileUploadCreationTest(ptc.PloneTestCase):
+
+    def test_dxfile_factory(self):
+        import StringIO
+        data = StringIO.StringIO()
+        data.write("Hello, world")
+        data.filename = 'file.txt'
+
+        factory = IDXFileFactory(self.portal)
+        new_object = factory('dx_file', 'File', data)
+        self.failUnless(IDexterityFTI.providedBy(new_object))
+        data.close()
 
 
-class FileUploadCreationTest(ptc.PloneTestCase):
+class ATCTFileUploadCreationTest(ptc.PloneTestCase):
 
     def test_atctfile_factory(self):
         factory = IATCTFileFactory(self.portal)
         new_object = factory('at_file', 'File', None)
         self.failUnless(IATFile.providedBy(new_object))
 
-    def test_dxfile_factory(self):
-        """ force dexterity to be installed """
-        pass
-        #factory = IDXFileFactory(self.portal)
-        #new_object = factory('dx_file', 'File', None)
-        #self.failUnless(IATFile.providedBy(new_object))
-
 def test_suite():
-    return unittest.makeSuite(FileUploadCreationTest)
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
