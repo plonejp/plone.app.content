c9aa1b2c8a40f924e91057dfc31686d0d8269458
vangheem <vangheem@gmail.com>
2015-07-11 16:11:38 -0500

    provide add context field to add form

--- plone/app/content/browser/folderfactories.py
+++ plone/app/content/browser/folderfactories.py
@@ -27,11 +27,50 @@ class FolderFactoriesView(BrowserView):
 
     def __call__(self):
         if 'form.button.Add' in self.request.form:
-            url = self.request.form.get('url')
-            self.request.response.redirect(url)
-            return ''
+            return self.redirect_to_add_form()
         else:
             return self.index()
+
+    def redirect_to_add_form(self):
+        type_id = self.request.form.get('id')
+        folder_path = self.request.form.get('folder')
+        if folder_path is None:
+            folder = self.add_context()
+        else:
+            portal = self.portal_state.portal()
+            if folder_path == '/':
+                folder = portal
+            else:
+                folder = portal.restrictedTraverse(folder_path.lstrip('/'))
+
+        for type_info in self.addable_types(addContext=folder):
+            if type_info['id'] == type_id:
+                self.request.response.redirect(type_info['action'])
+                return ''
+        # XXX This should NOT happen. validation, etc should hapeen and users
+        # should not see this
+        raise Exception("Not allowed to add type to this container")
+
+    @property
+    @memoize
+    def portal_state(self):
+        return getMultiAdapter((self.context, self.request),
+                               name='plone_portal_state')
+
+    @property
+    @memoize
+    def context_state(self):
+        return getMultiAdapter((self.context, self.request),
+                               name='plone_context_state')
+    @property
+    def current_folder(self):
+        if self.context_state.is_portal_root():
+            return '/'
+        portal = self.portal_state.portal()
+        site_path = '/'.join(portal.getPhysicalPath())
+        context_path = '/'.join(self.context_state.folder().getPhysicalPath())
+        path = context_path[len(site_path):]
+        return path
 
     def can_constrain_types(self):
         aspect = ISelectableConstrainTypes(self.add_context(), None)
@@ -40,15 +79,13 @@ class FolderFactoriesView(BrowserView):
     @memoize
     def add_context(self):
         context = self.context
-        context_state = getMultiAdapter((context, self.request),
-                                        name='plone_context_state')
         context = aq_inner(context)
         try:
             published = self.request.PUBLISHED
         except AttributeError:
             published = context
-        if context_state.is_structural_folder():
-            if context_state.is_default_page():
+        if self.context_state.is_structural_folder():
+            if self.context_state.is_default_page():
                 is_folder_contents_view = \
                     IFolderContentsView.providedBy(published)
                 if is_folder_contents_view or self == published:
@@ -61,11 +98,11 @@ class FolderFactoriesView(BrowserView):
                 return context
         else:
             return aq_parent(context)
+
     # NOTE: This is also used by plone.app.contentmenu.menu.FactoriesMenu.
     # The return value is somewhat dictated by the menu infrastructure, so
     # be careful if you change it
-
-    def addable_types(self, include=None):
+    def addable_types(self, include=None, addContext=None):
         """Return menu item entries in a TAL-friendly form.
 
         Pass a list of type ids to 'include' to explicitly allow a list of
@@ -78,10 +115,9 @@ class FolderFactoriesView(BrowserView):
         results = []
 
         idnormalizer = queryUtility(IIDNormalizer)
-        portal_state = getMultiAdapter((context, request),
-                                       name='plone_portal_state')
 
-        addContext = self.add_context()
+        if addContext is None:
+            addContext = self.add_context()
         baseUrl = addContext.absolute_url()
         token = createToken()
 
@@ -101,7 +137,7 @@ class FolderFactoriesView(BrowserView):
         addActionsById = dict([(a['id'], a) for a in actions])
 
         expr_context = createExprContext(
-            aq_parent(addContext), portal_state.portal(), addContext)
+            aq_parent(addContext), self.portal_state.portal(), addContext)
         for t in allowedTypes:
             typeId = t.getId()
             if include is None or typeId in include:
